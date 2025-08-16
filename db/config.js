dfc5e6d74f6d56ad1fcd2a506b73489a6cbd075e
Quiros Benjamin <bq170921@gmail.com>
2025-08-16 16:08:22 -0300

    configuracion de base de datos

--- db/config.js
+++ db/config.js
@@ -2,6 +2,7 @@ import mongoose from "mongoose";
 try {
     mongoose.connect(process.env.MONGODB).then(()=>{
         console.info("base de datos completada")
+        //aqui vemos si se conecta a mongo db
     })
 } catch (error) {
     console.error(error)
