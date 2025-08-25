5dd68827fb0d82c16c9905bb4e06550b69952190
Quiros Benjamin <bq170921@gmail.com>
2025-08-25 15:06:11 -0300

    fix 2

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-/*import Server from "./server/config.js";
+import Server from "./server/config.js";
 import routes from "./src/routes/index.routes.js"; // archivo que junta todas las rutas
 
 const server = new Server();
 server.app.use("/api", routes);
-server.listen();*/
+server.listen();
+/*
 import Server from "./server/config.js";
 import routes from "./src/routes/index.routes.js";
 
@@ -13,3 +14,4 @@ server.app.use("/api", routes);
 // ❌ NO usamos listen
 // ✅ Exportamos la app para que Vercel la ejecute como función serverless
 export default server.app;
+*/
